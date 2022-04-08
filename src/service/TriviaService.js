4897121592da3b93eb156ba958223517b96b8ce4
AlexSensei <antishadow15@gmail.com>
2022-04-08 22:01:07 +0200

    Added trivia clue category selector

--- src/service/TriviaService.js
+++ src/service/TriviaService.js
@@ -1,8 +1,21 @@
 import HttpService from "./HttpService";
 
 class TriviaService extends HttpService {
-  getClues = async () => {
-    const { data } = await this.client.get("http://jservice.io/api/clues");
+  getClues = async (category) => {
+    const { data } = await this.client.get("http://jservice.io/api/clues", {
+      params: { category },
+    });
+
+    return data;
+  };
+
+  getCategories = async () => {
+    const { data } = await this.client.get(
+      "http://jservice.io/api/categories",
+      {
+        params: { count: 10 },
+      }
+    );
 
     return data;
   };
