e5530e4b846c874eddbc2c13870823c589501a5c
AlexSensei <antishadow15@gmail.com>
2022-04-08 21:47:09 +0200

    Added trivia clues fetching and rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Chuck from "./components/Chuck";
+import { Trivia } from "./components/Trivia";
 
 function App() {
-  const [trivia, setTrivia] = useState(false);
+  const [trivia, setTrivia] = useState(true);
   return (
     <div>
       <button onClick={() => setTrivia(true)}>Trivia</button>
       <button onClick={() => setTrivia(false)}>Chuck</button>
-      {trivia ? <p>Trivia</p> : <Chuck />}
+      {trivia ? <Trivia /> : <Chuck />}
     </div>
   );
 }
---
+++ src/service/TriviaService.js
@@ -0,0 +1,11 @@
+import HttpService from "./HttpService";
+
+class TriviaService extends HttpService {
+  getClues = async () => {
+    const { data } = await this.client.get("http://jservice.io/api/clues");
+
+    return data;
+  };
+}
+
+export default new TriviaService();
